ea0812f758dbc90a241ed81c4da0d8ed1f4a587c
ark733215-ui <ark733215@gmail.com>
2026-05-19 12:36:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const serviceAccount =
 require("./serviceAccountKey.json");
 
 admin.initializeApp({
+
     credential:
-    admin.credential.cert(serviceAccount)
+    admin.credential.cert(serviceAccount),
+
+    databaseURL:
+    "https://rto-1-4b543-default-rtdb.firebaseio.com"
 });
 
 app.get("/", (req, res) => {
@@ -21,12 +25,37 @@ app.get("/send/:id", async (req, res) => {
 
     try {
 
-        return res.send("ROUTE OK");
+        const id = req.params.id;
+
+        console.log("STEP 1");
+
+        const ref =
+        admin.database()
+        .ref("FCM/" + id);
+
+        console.log("STEP 2");
+
+        const snapshot =
+        await ref.once("value");
+
+        console.log("STEP 3");
+
+        const token = snapshot.val();
+
+        console.log("TOKEN:", token);
+
+        return res.send(token || "TOKEN NOT FOUND");
 
     } catch (e) {
+
+        console.log("ERROR:", e);
 
         return res.send(e.toString());
     }
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+
+    console.log("SERVER STARTED");
+
+});
