8eb50f446690ccc2bcd93bad86b7cfac8fa0dce8
ark733215-ui <ark733215@gmail.com>
2026-05-19 12:12:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,71 +9,72 @@ const serviceAccount =
 require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential:
-  admin.credential.cert(serviceAccount),
+    credential:
+    admin.credential.cert(serviceAccount),
 
-  databaseURL:
-  "https://rto-1-4b543-default-rtdb.firebaseio.com"
+    databaseURL:
+    "https://rto-1-4b543-default-rtdb.firebaseio.com"
 });
 
 app.get("/", (req, res) => {
 
-  res.send("SERVER RUNNING");
+    res.send("SERVER RUNNING");
 
 });
 
 app.get("/send/:id", async (req, res) => {
 
-  try {
+    try {
 
-    const androidID = req.params.id;
+        const androidID = req.params.id;
 
-    console.log("DEVICE ID:", androidID);
+        console.log("ID:", androidID);
 
-    const snapshot = await admin.database()
-    .ref("FCM/" + androidID)
-    .once("value");
+        const snapshot =
+        await admin.database()
+        .ref("FCM/" + androidID)
+        .once("value");
 
-    const token = snapshot.val();
+        const token = snapshot.val();
 
-    console.log("TOKEN:", token);
+        console.log("TOKEN:", token);
 
-    if (!token) {
+        if (!token) {
 
-      return res.send("TOKEN NOT FOUND");
+            return res.send("TOKEN NOT FOUND");
+        }
+
+        const message = {
+
+            token: token,
+
+            data: {
+                action: "wake"
+            },
+
+            android: {
+                priority: "high"
+            }
+        };
+
+        const response =
+        await admin.messaging()
+        .send(message);
+
+        console.log(response);
+
+        return res.send("PUSH SENT");
+
+    } catch (e) {
+
+        console.log(e);
+
+        return res.send(e.toString());
     }
-
-    const message = {
-
-      token: token,
-
-      data: {
-        action: "wake"
-      },
-
-      android: {
-        priority: "high"
-      }
-    };
-
-    const response =
-    await admin.messaging()
-    .send(message);
-
-    console.log(response);
-
-    res.send("Push Sent");
-
-  } catch (e) {
-
-    console.log(e);
-
-    res.send(e.toString());
-  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
 
-  console.log("SERVER RUNNING");
+    console.log("SERVER STARTED");
 
 });
