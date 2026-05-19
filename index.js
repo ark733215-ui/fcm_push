11b4d0e5f89a758c24b57440cb0468668206937d
ark733215-ui <ark733215@gmail.com>
2026-05-19 13:47:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 
 const app = express();
 
-const serviceAccount = require("./fuckyou.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
