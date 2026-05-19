7572b1f0af281b1e007876496c7594ea0a792b5d
ark733215-ui <ark733215@gmail.com>
2026-05-19 13:30:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,58 +3,35 @@ const admin = require("firebase-admin");
 
 const app = express();
 
-const serviceAccount =
-require("./serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential:
-    admin.credential.cert(serviceAccount),
-
-  databaseURL:
-    "https://rto-1-4b543-default-rtdb.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://rto-1-4b543-default-rtdb.firebaseio.com"
 });
 
 app.get("/", (req, res) => {
   res.send("SERVER RUNNING");
 });
 
-app.get("/send/:id", async (req, res) => {
+app.get("/test", async (req, res) => {
 
   try {
 
-    const id = req.params.id;
+    await admin.database().ref("test").set("hello");
 
-    console.log("STEP 1");
-
-    const snapshot = await Promise.race([
-
-      admin.database()
-      .ref("FCM/" + id)
-      .once("value"),
-
-      new Promise((_, reject) =>
-        setTimeout(() =>
-          reject(new Error("FIREBASE TIMEOUT")), 10000)
-      )
-
-    ]);
-
-    console.log("STEP 2");
-
-    if (!snapshot.exists()) {
-      return res.send("TOKEN NOT FOUND");
-    }
-
-    return res.send(snapshot.val());
+    res.send("FIREBASE CONNECTED");
 
   } catch (e) {
 
-    console.log("ERROR:", e);
+    res.send(e.toString());
 
-    return res.send(e.toString());
   }
+
 });
 
-app.listen(process.env.PORT || 3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Server Running");
 });
