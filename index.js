a34c190c082f2eee019d22d27ce5a8abc9ae25e1
ark733215-ui <ark733215@gmail.com>
2026-05-19 12:26:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,19 @@
 const express = require("express");
-const admin = require("firebase-admin");
 
 const app = express();
 
-const serviceAccount =
-require("./serviceAccountKey.json");
-
-admin.initializeApp({
-
-    credential:
-    admin.credential.cert(serviceAccount),
-
-    databaseURL:
-    "https://rto-1-4b543-default-rtdb.firebaseio.com"
-});
-
 app.get("/", (req, res) => {
 
-    res.send("SERVER RUNNING");
+    res.send("HOME OK");
 
 });
 
-app.get("/send/:id", async (req, res) => {
+app.get("/send/:id", (req, res) => {
 
-    try {
+    const id = req.params.id;
 
-        const androidID = req.params.id;
+    return res.send("ID = " + id);
 
-        console.log("ID:", androidID);
-
-        const snapshot =
-        await admin.database()
-        .ref("FCM/" + androidID)
-        .once("value");
-
-        const token = snapshot.val();
-
-        console.log("TOKEN:", token);
-
-        if (!token) {
-
-            return res.send("TOKEN NOT FOUND");
-        }
-
-        return res.send(token);
-
-    } catch (e) {
-
-        console.log(e);
-
-        return res.send(e.toString());
-    }
 });
 
 app.listen(process.env.PORT || 3000, () => {
